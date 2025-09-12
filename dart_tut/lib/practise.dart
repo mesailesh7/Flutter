a204c02bce6ac3b22708833ddc1ca4cb90d61674
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-09-12 15:02:33 -0600

    data types

--- dart_tut/lib/practise.dart
+++ dart_tut/lib/practise.dart
@@ -2,15 +2,36 @@ import 'dart:io';
 
 void main() {
   // print("This is hello world");
-  stdout.write('Enter your name: ');
-  var name = stdin.readLineSync();
-  print("Welcome, $name");
+
+  // stdout.write('Enter your name: ');
+  // var name = stdin.readLineSync();
+  // print("Welcome, $name");
 
   //every time a class is used . Default constructor is called right away
-  var raman = new Human();
+  // var raman = new Human();
+  // new Human();
+  Human(); //Creating a class object
+
+  int? a;
+  a = 5;
+  print(a);
+
+  BigInt longValue;
+  longValue = BigInt.parse('5555555555555555');
+  // print(longValue.runtimeType);
+  print(longValue);
+
+  var section;
+
+  section = "D";
+  section = 7;
+  section = false;
+
+  print(section);
 }
 
 class Human {
   // default constructor
+
   Human();
 }

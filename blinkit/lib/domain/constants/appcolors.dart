c6ea6538c9e5303b836181be79e51730c33c78ae
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-09-23 09:26:29 -0600

    Splash Screen and Assets

---
+++ blinkit/lib/domain/constants/appcolors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color scaffoldbackground = Color(0xfff7cb45);
+}
---
+++ blinkit/lib/repository/screens/splash/splashscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Splashscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
